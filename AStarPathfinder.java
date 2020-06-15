f75cc10fd82ff8d3a5827c04a2ae1b21f55e26b1
killso <61009092+killso@users.noreply.github.com>
2020-06-15 18:51:34 +0300

    Add files via upload

---
+++ AStarPathfinder.java
@@ -0,0 +1,106 @@
+import java.util.HashMap;
+import java.util.HashSet;
+/**
+ * This class contains the implementation of the A* pathfinding algorithm.  The
+ * algorithm is implemented as a static method, since the pathfinding algorithm
+ * really doesn't need to maintain any state between invocations of the
+ * algorithm.
+ */
+public class AStarPathfinder
+{
+    /**
+     * This constant holds a maximum cutoff limit for the cost of paths.  If a
+     * particular waypoint happens to exceed this cost limit, the waypoint is
+     * discarded.
+     **/
+    public static final float COST_LIMIT = 1e6f;
+    /**
+     * Attempts to compute a path that navigates between the start and end
+     * locations of the specified map.  If a path can be found, the waypoint of
+     * the <em>final</em> step in the path is returned; that waypoint can be
+     * used to walk backwards to the starting point.  If no path can be found,
+     * <code>null</code> is returned.
+     **/
+    //добавляем
+    public static Waypoint Map (Map2D map)
+    {
+        AStarState state = new AStarState(map);
+        Location finish = map.getFinish();
+        Waypoint start = new Waypoint(map.getStart(), null);
+        start.setCosts(0, estimateTravelCost(start.getLocation(), finish));
+        state.addOpenWaypoint(start);
+        Waypoint finalWaypoint = null;
+        boolean foundPath = false;
+        while (!foundPath && state.numOpenWaypoints() > 0)
+        {
+            Waypoint best = state.getMinOpenWaypoint();
+            if (best.getLocation().equals(finish))
+            {
+                finalWaypoint = best;
+                foundPath = true;
+            }
+            takeNextStep(best, state);
+            state.closeWaypoint(best.getLocation());
+        }
+        return finalWaypoint;
+    }
+    /**
+     * This static helper method takes a waypoint, and generates all valid "next
+     * steps" from that waypoint.  The new waypoints are added to the "open
+     * waypoints" collection of the passed-in A* state object.
+     **/
+    private static void takeNextStep(Waypoint currWP, AStarState state)
+    {
+        Location loc = currWP.getLocation();
+        Map2D map = state.getMap();
+        for (int y = loc.yCoord - 1; y <= loc.yCoord + 1; y++)
+        {
+            for (int x = loc.xCoord - 1; x <= loc.xCoord + 1; x++)
+            {
+                Location nextLoc = new Location(x, y);
+                // If "next location" is outside the map, skip it.
+                if (!map.contains(nextLoc))
+                    continue;
+                // If "next location" is this location, skip it.
+                if (nextLoc == loc)
+                    continue;
+                // If this location happens to already be in the "closed" set
+                // then continue on with the next location.
+                if (state.isLocationClosed(nextLoc))
+                    continue;
+                // Make a waypoint for this "next location."
+                Waypoint nextWP = new Waypoint(nextLoc, currWP);
+                // OK, we cheat and use the cost estimate to compute the actual
+                // cost from the previous cell.  Then, we add in the cost from
+                // the map cell we step onto, to incorporate barriers etc.
+                float prevCost = currWP.getPreviousCost() +
+                        estimateTravelCost(currWP.getLocation(),
+                                nextWP.getLocation());
+                prevCost += map.getCellValue(nextLoc);
+                // Skip this "next location" if it is too costly.
+                if (prevCost >= COST_LIMIT)
+                    continue;
+                nextWP.setCosts(prevCost,
+                        estimateTravelCost(nextLoc, map.getFinish()));
+
+                // Add the waypoint to the set of open waypoints.  If there
+                // happens to already be a waypoint for this location, the new
+                // waypoint only replaces the old waypoint if it is less costly
+                // than the old one.
+                state.addOpenWaypoint(nextWP);
+            }
+        }
+    }
+
+    /**
+     * Estimates the cost of traveling between the two specified locations.
+     * The actual cost computed is just the straight-line distance between the
+     * two locations.
+     **/
+    private static float estimateTravelCost(Location currLoc, Location destLoc)
+    {
+        int dx = destLoc.xCoord - currLoc.xCoord;
+        int dy = destLoc.yCoord - currLoc.yCoord;
+        return (float) Math.sqrt(dx * dx + dy * dy);
+    }
+}
